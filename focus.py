0745c4a4b6e8dc1d217061a7c6541fdb95714a6f
Adigo10 <adityagoel1999@gmail.com>
2020-05-23 03:35:03 +0530

    Alert frame 'count' increaments when face not detected.

--- focus.py
+++ focus.py
@@ -24,7 +24,7 @@ def eye_aspect_ratio(eye):
 #alert threshold for eye
 EYE_AR_THRESH = 0.30
 #No. of frames after which alert will work
-EYE_AR_CONSEC_FRAMES = 100
+EYE_AR_CONSEC_FRAMES = 120
 
 # initialize the frame count as well as a boolean used to
 # indicate if the alarm is going off
@@ -60,95 +60,104 @@ while True:
     # detect faces in the grayscale frame
     rects = detector(gray, 0)
 
-    # loop over the face detections
-    for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+    # when face not detected
+    if(len(rects) == 0):
+        count += 1
+        print(count)
 
-        # for headpose
-        #2D image points. If you change the image, you need to change vector
-        image_points = np.array([
-                                    (shape[33, :]),     # Nose tip
-                                    (shape[8,  :]),     # Chin
-                                    (shape[36, :]),     # Left eye left corner
-                                    (shape[45, :]),     # Right eye right corne
-                                    (shape[48, :]),     # Left Mouth corner
-                                    (shape[54, :])      # Right mouth corner
-                                ], dtype="double")
+    else:
 
-        # 3D model points.
-        model_points = np.array([
-                                    (0.0, 0.0, 0.0),             # Nose tip
-                                    (0.0, -330.0, -65.0),        # Chin
-                                    (-225.0, 170.0, -135.0),     # Left eye left corner
-                                    (225.0, 170.0, -135.0),      # Right eye right corne
-                                    (-150.0, -150.0, -125.0),    # Left Mouth corner
-                                    (150.0, -150.0, -125.0)      # Right mouth corner
-                                ])
+	    # loop over the face detections
+	    for rect in rects:
+	        # determine the facial landmarks for the face region, then
+	        # convert the facial landmark (x, y)-coordinates to a NumPy
+	        # array
+	        shape = predictor(gray, rect)
+	        shape = face_utils.shape_to_np(shape)
 
-        # Camera internals
-        size = frame.shape
-        focal_length = size[1]
-        center = (size[1]//2, size[0]//2)
-        camera_matrix = np.array(
-                                 [[focal_length, 0, center[0]],
-                                 [0, focal_length, center[1]],
-                                 [0, 0, 1]], dtype = "double"
-                                 )
-        #print ("Camera Matrix :\n {0}".format(camera_matrix))
+	        # for headpose
+	        #2D image points. If you change the image, you need to change vector
+	        image_points = np.array([
+	                                    (shape[33, :]),     # Nose tip
+	                                    (shape[8,  :]),     # Chin
+	                                    (shape[36, :]),     # Left eye left corner
+	                                    (shape[45, :]),     # Right eye right corne
+	                                    (shape[48, :]),     # Left Mouth corner
+	                                    (shape[54, :])      # Right mouth corner
+	                                ], dtype="double")
 
-        dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
-        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
-        #print ("Rotation Vector:\n {0}".format(rotation_vector))
-        #print ("Translation Vector:\n {0}".format(translation_vector))
+	        # 3D model points.
+	        model_points = np.array([
+	                                    (0.0, 0.0, 0.0),             # Nose tip
+	                                    (0.0, -330.0, -65.0),        # Chin
+	                                    (-225.0, 170.0, -135.0),     # Left eye left corner
+	                                    (225.0, 170.0, -135.0),      # Right eye right corne
+	                                    (-150.0, -150.0, -125.0),    # Left Mouth corner
+	                                    (150.0, -150.0, -125.0)      # Right mouth corner
+	                                ])
 
-        # Project a 3D point (0, 0, 1000.0) onto the frame.
-        # We use this to draw a line sticking out of the nose
-        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+	        # Camera internals
+	        size = frame.shape
+	        focal_length = size[1]
+	        center = (size[1]//2, size[0]//2)
+	        camera_matrix = np.array(
+	                                 [[focal_length, 0, center[0]],
+	                                 [0, focal_length, center[1]],
+	                                 [0, 0, 1]], dtype = "double"
+	                                 )
+	        #print ("Camera Matrix :\n {0}".format(camera_matrix))
 
-        for p in image_points:
-            cv2.circle(frame, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
-        p0 = ( int(image_points[0][0]), int(image_points[0][1]) - 2)
-        p1 = ( int(image_points[0][0]), int(image_points[0][1]))
-        p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+	        dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+	        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+	        #print ("Rotation Vector:\n {0}".format(rotation_vector))
+	        #print ("Translation Vector:\n {0}".format(translation_vector))
 
-        cv2.line(frame, p1, p2, (255,0,0), 2)
-        angle = cal_angle(p0,p1,p2)
-        print(angle)
+	        # Project a 3D point (0, 0, 1000.0) onto the frame.
+	        # We use this to draw a line sticking out of the nose
+	        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
 
-		#for eye
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
+	        for p in image_points:
+	            cv2.circle(frame, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+	        p0 = ( int(image_points[0][0]), int(image_points[0][1]) - 2)
+	        p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+	        p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
 
-        # average of eye aspect ratio
-        ear = (leftEAR + rightEAR) / 2.0
+	        cv2.line(frame, p1, p2, (255,0,0), 2)
+	        angle = cal_angle(p0,p1,p2)
 
-        # visualize the convex hull for the left and right eye
 
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+			#for eye
+	        # extract the left and right eye coordinates, then use the
+	        # coordinates to compute the eye aspect ratio for both eyes
+	        leftEye = shape[lStart:lEnd]
+	        rightEye = shape[rStart:rEnd]
+	        leftEAR = eye_aspect_ratio(leftEye)
+	        rightEAR = eye_aspect_ratio(rightEye)
 
-        #if eye aspect ratio is below the blink threshold increment the blink count
-        if (ear < EYE_AR_THRESH or angle < -70 or angle >70):
-            count += 1
-            print (count)
-            if count >= EYE_AR_CONSEC_FRAMES:
+	        # average of eye aspect ratio
+	        ear = (leftEAR + rightEAR) / 2.0
 
-                cv2.putText(frame, "!!!!FOCUS ON THE SCREEN!!!!", (10, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+	        # visualize the convex hull for the left and right eye
 
-        #the eye aspect ratio is not below the blink threshold, so reset the count
-        else:
-            count = 0
+	        leftEyeHull = cv2.convexHull(leftEye)
+	        rightEyeHull = cv2.convexHull(rightEye)
+	        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+	        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+	        #if eye aspect ratio is below the blink threshold increment the blink count
+	        if (ear < EYE_AR_THRESH or angle < -70 or angle >70):
+	            count += 1
+	            print (count)
+	            #print (ear)
+	            print(angle)
+	            if count >= EYE_AR_CONSEC_FRAMES:
+
+	                cv2.putText(frame, "!!!!FOCUS ON THE SCREEN!!!!", (10, 30),
+	                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+	        #the eye aspect ratio is not below the blink threshold, so reset the count
+	        else:
+	            count = 0
 
         #computed eye aspect ratio on the frame
         #cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
