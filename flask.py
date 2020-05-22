2992864ae1d777ca048ba002d976f9037b7561f9
v007rj <41225554+v007rj@users.noreply.github.com>
2020-05-22 17:32:35 +0530

    Falsk

---
+++ flask.py
@@ -0,0 +1,158 @@
+
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import imutils
+import time
+import dlib
+import cv2
+def model_predict():
+
+
+ def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    # compute the euclidean distance between the horizontal eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+    # return the eye aspect ratio
+    return ear
+
+ #alert threshold for eye
+ EYE_AR_THRESH = 0.30
+ #No. of frames after which alert will work
+ EYE_AR_CONSEC_FRAMES = 120
+
+ # initialize the frame counter as well as a boolean used to
+ # indicate if the alarm is going off
+ COUNTER = 0
+ ALARM_ON = False
+
+ # initializing dlib's shape_predictor_68_face_landmarks.dat and then create the facial landmark predictor
+ detector = dlib.get_frontal_face_detector()
+ predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+ # grab the indexes of the facial landmarks for the left and
+ # right eye, respectively
+ (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+ (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+ # start the video stream thread
+ print("starting video stream thread...")
+ vs = cv2.VideoCapture(0)
+ time.sleep(1.0)
+
+ # loop over frames from the video stream
+ while True:
+    # grab the frame from the threaded video file stream, resize
+    # it, and convert it to grayscale
+    # channels)
+    _,frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+
+    # loop over the face detections
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        # draw face Contours
+        for (x, y) in shape:
+            cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+
+        # extract the left and right eye coordinates, then use the
+        # coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+
+        # compute the convex hull for the left and right eye, then
+        # visualize each of the eyes
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+            # if the eyes were closed for a sufficient number of frames
+
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+
+                cv2.putText(frame, "FOCUS ALERT!", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        # otherwise, the eye aspect ratio is not below the blink
+        # threshold, so reset the counter and alarm
+        else:
+            COUNTER = 0
+
+        # draw the computed eye aspect ratio on the frame to help
+        # with debugging and setting the correct eye aspect ratio
+        # thresholds and frame counters
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    # show the frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(50) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+ # do a bit of cleanup
+ cv2.destroyAllWindows()
+ vs.release()
+    
+     
+
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Define a flask app
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+#import inspect
+#src_file_path = inspect.getfile(lambda: None)
+@app.route('/predict', methods=['GET', 'POST'])
+def cameraon():
+    model_predict()
+    
+
+if __name__ == '__main__':
+    # app.run(port=5002, debug=True)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+    app.run()
