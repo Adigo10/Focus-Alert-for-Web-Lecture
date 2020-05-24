14c3ad88ce1037e59155e0efcc8ac9403f058925
Adigo10 <adityagoel1999@gmail.com>
2020-05-24 19:37:19 +0530

    flask update

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(name=None):
+    return render_template('index.html',name=name)
+
+@app.route('/exec')
+def parse(name=None):
+    import focus
+    return render_template('index.html',name=name)
+
+if __name__ == '__main__':
+    app.run()
+    app.debug = True
